c4c3968c524471ca1f715a218ece7c551c2a59bd
myeongjunkim <ilikerunning@naver.com>
2022-09-29 13:43:24 +0900

    fix: error 이름 수정

--- onboarding_app/main.py
+++ onboarding_app/main.py
@@ -3,12 +3,14 @@ from fastapi.responses import JSONResponse
 
 from onboarding_app import exceptions
 from onboarding_app.database import Base, engine
+from onboarding_app.endpoints.stock import stock_router
 from onboarding_app.endpoints.user import user_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(user_router)
+app.include_router(stock_router)
 
 
 @app.exception_handler(exceptions.CredentialsError)
@@ -22,13 +24,13 @@ async def credentialsError_exception_handler(
     )
 
 
-@app.exception_handler(exceptions.UserDuplicatedError)
+@app.exception_handler(exceptions.DuplicatedError)
 async def duplicated_exception_handler(
-    request: Request, exc: exceptions.UserDuplicatedError
+    request: Request, exc: exceptions.DuplicatedError
 ):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content="User info is duplicated",
+        content="Data info is duplicated",
     )
 
 
@@ -42,9 +44,9 @@ async def data_does_not_exist_exception_handler(
     )
 
 
-@app.exception_handler(exceptions.DataDoesNotExistError)
+@app.exception_handler(exceptions.InactiveUserError)
 async def inactive_user_exception_handler(
-    request: Request, exc: exceptions.DataDoesNotExistError
+    request: Request, exc: exceptions.InactiveUserError
 ):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
