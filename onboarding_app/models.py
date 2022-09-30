50034f71a45292dd7f0da87466fa9f28f250b54e
myeongjunkim <ilikerunning@naver.com>
2022-09-30 14:44:48 +0900

    feat: add wihslist model

--- onboarding_app/models.py
+++ onboarding_app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from onboarding_app.database import Base, engine
 
@@ -13,6 +15,8 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
 
+    wishlist = relationship("Wishlist", back_populates="users")
+
 
 class Stock(Base):
     __tablename__ = "stocks"
@@ -26,3 +30,18 @@ class Stock(Base):
 
 User.__table__.create(bind=engine, checkfirst=True)
 Stock.__table__.create(bind=engine, checkfirst=True)
+
+
+class Wishlist(Base):
+    __tablename__ = "wishlists"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    name = Column(String, index=True)
+    description = Column(String)
+    created_date = Column(DateTime(timezone=True), server_default=func.now())
+    updated_date = Column(DateTime(timezone=True), onupdate=func.now())
+    is_open = Column(Boolean, default=True)
+    order_method = Column(Integer)
+
+    user = relationship("User", back_populates="wishlists")
