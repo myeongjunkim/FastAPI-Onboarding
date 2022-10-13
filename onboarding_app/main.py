1e5eaa0cc73f759303be006546c6dd83c0c1a858
myeongjunkim <ilikerunning@naver.com>
2022-10-13 09:51:20 +0900

    fix: edit content of duplicated_exception_handler

--- onboarding_app/main.py
+++ onboarding_app/main.py
@@ -30,7 +30,7 @@ async def duplicated_exception_handler(
 ):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content="User info is duplicated",
+        content="Data is duplicated",
     )
 
 
