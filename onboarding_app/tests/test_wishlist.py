7306a194e77a2c419cf00bea7673900d3c116a52
myeongjunkim <ilikerunning@naver.com>
2022-10-24 16:39:55 +0900

    fix: globalize db var and refactor test_change_wishlist_order

--- onboarding_app/tests/test_wishlist.py
+++ onboarding_app/tests/test_wishlist.py
@@ -1,12 +1,15 @@
+import pytest
+
 from onboarding_app import models, schemas
 from onboarding_app.queries import user as user_query, wishlist as wishlist_query
 from onboarding_app.tests.conftest import client, TestingSessionLocal
 from onboarding_app.tests.utils import get_wishlist_by_name, obtain_token_reg
 
+db = TestingSessionLocal()
+
 
 def test_wishlists_create_success():
     # Given
-    db = TestingSessionLocal()
     reg1_token = obtain_token_reg("reg1")
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
 
@@ -30,7 +33,6 @@ def test_wishlists_create_success():
 def test_wishlists_fetch_success():
     # Given
     reg1_token = obtain_token_reg("reg1")
-    db = TestingSessionLocal()
     reg1 = user_query.get_user_by_username(db=TestingSessionLocal(), username="reg1")
 
     for i in range(10):
@@ -56,7 +58,6 @@ def test_wishlists_fetch_success():
 
 def test_wishlists_get_success():
     # Given
-    db = TestingSessionLocal()
     reg1_token = obtain_token_reg("reg1")
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
 
@@ -85,7 +86,6 @@ def test_wishlists_get_fail_with_other_user_token():
     # Given
     other_user_token = obtain_token_reg("reg2")
 
-    db = TestingSessionLocal()
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
     wishlist = wishlist_query.create_wishlist(
         db=db,
@@ -108,7 +108,6 @@ def test_wishlists_get_fail_with_other_user_token():
 
 def test_wishlists_update_success():
     # Given
-    db = TestingSessionLocal()
     reg1_token = obtain_token_reg("reg1")
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
 
@@ -142,7 +141,6 @@ def test_wishlists_update_success():
 
 def test_wishlists_delete_success():
     # Given
-    db = TestingSessionLocal()
     reg1_token = obtain_token_reg("reg1")
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
 
@@ -174,7 +172,6 @@ def test_wishlists_update_and_delete_fail_with_other_user_token():
     # Given
     other_user_token = obtain_token_reg("reg2")
 
-    db = TestingSessionLocal()
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
     wishlist = wishlist_query.create_wishlist(
         db=db,
@@ -204,9 +201,17 @@ def test_wishlists_update_and_delete_fail_with_other_user_token():
     assert wishlist_delete_response_by_other_uesr.status_code == 401
 
 
-def test_change_wishlist_order():
+@pytest.mark.parametrize(
+    "origin_order, hope_order",
+    (
+        # 범위 내에서 정렬
+        (2, 7),  # Case 1. 낮은 순서에서 높은 순서로 올라가는 경우
+        (5, 2),  # Case 2. 높은 순서에서 낮은 순서로 내려가는 경우
+        (3, 3),  # Case 3. 같은 순서로 정렬하는 경우
+    ),
+)
+def test_change_wishlist_order(origin_order: int, hope_order: int):
     # Given
-    db = TestingSessionLocal()
     reg1_token = obtain_token_reg("reg1")
     reg1 = user_query.get_user_by_username(db=db, username="reg1")
 
@@ -219,14 +224,13 @@ def test_change_wishlist_order():
                 description=f"wishlist{i} description",
             ),
         )
-    db_wishlist = get_wishlist_by_name(db=db, name="wishlist2", current_user=reg1)
-
-    origin_order = 2
-    hope_order = 5
+    target_wishlist = get_wishlist_by_name(
+        db=db, name=f"wishlist{origin_order}", current_user=reg1
+    )
 
     # When
     wishlist_order_response = client.put(
-        f"/wishlists/{db_wishlist.id}/order",
+        f"/wishlists/{target_wishlist.id}/order",
         headers={"Authorization": "Bearer " + reg1_token},
         params={"hope_order": hope_order},
     )
@@ -235,13 +239,21 @@ def test_change_wishlist_order():
     assert wishlist_order_response.status_code == 200
     assert wishlist_order_response.json()["order_num"] == hope_order
 
-    for i in range(10):
-        db_wishlist = get_wishlist_by_name(
-            db=db, name=f"wishlist{i}", current_user=reg1
+    wishlists_ordered_by_order_num = (
+        db.query(models.Wishlist)
+        .filter(
+            models.Wishlist.user_id == reg1.id,
         )
-        if i < origin_order or i > hope_order:
-            assert db_wishlist.order_num == i
-        elif i == origin_order:
-            assert db_wishlist.order_num == hope_order
+        .order_by(models.Wishlist.order_num)
+        .all()
+    )
+
+    for i, wishlist in enumerate(wishlists_ordered_by_order_num):
+
+        if i < min(origin_order, hope_order) or i > max(origin_order, hope_order):
+            assert wishlist.name == f"wishlist{i}"
+        elif i == hope_order:
+            assert wishlist.name == f"wishlist{origin_order}"
         else:
-            assert db_wishlist.order_num == i - 1
+            gap = 1 if origin_order < hope_order else -1
+            assert wishlist.name == f"wishlist{i + gap}"
